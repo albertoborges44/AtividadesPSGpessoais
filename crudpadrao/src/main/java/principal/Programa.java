75cf1360be642bf238fcac632c9aea865d7c5f18
albertoborges44 <90792748+albertoborges44@users.noreply.github.com>
2021-10-15 17:31:09 -0400

    Add files via upload

---
+++ crudpadrao/src/main/java/principal/Programa.java
@@ -0,0 +1,23 @@
+package principal;
+
+import java.util.Date;
+
+import dominio.Categoria;
+import repositorio.CategoriaRepositorio;
+import servico.CategoriaServico;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		CategoriaRepositorio repo = new CategoriaRepositorio("exemplo-jpa");
+	
+		CategoriaServico servico = new CategoriaServico(repo);
+		
+		for (Categoria item : servico.listar()) {
+			System.out.println(item);
+		}
+		
+		repo.dispose();
+
+	}
+}
