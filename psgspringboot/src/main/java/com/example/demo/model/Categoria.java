7b7bac89ec6bfe398f3dddfbfa4b779163467e01
albertoborges44 <90792748+albertoborges44@users.noreply.github.com>
2021-10-21 17:13:50 -0400

    Add files via upload

---
+++ psgspringboot/src/main/java/com/example/demo/model/Categoria.java
@@ -0,0 +1,72 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "CategoriaID")
+	private Integer CategoriaId;
+	
+	@Column(name = "Descricao")
+	private String descricao;
+	
+	@Column(name = "Inclusao", nullable = true)
+	@Temporal(value = TemporalType.DATE)
+	private Date dataDeInsercao;
+
+	public Categoria() {
+		
+	}
+	
+	public Integer getCategoriaId() {
+		return CategoriaId;
+	}
+
+	public void setCategoriaId(Integer categoriaId) {
+		CategoriaId = categoriaId;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataDeInsercao() {
+		return dataDeInsercao;
+	}
+
+	public void setDataDeInsercao(Date dataDeInsercao) {
+		this.dataDeInsercao = dataDeInsercao;
+	}
+
+	public Categoria(Integer categoriaId, String descricao, Date dataDeInsercao) {
+		super();
+		CategoriaId = categoriaId;
+		this.descricao = descricao;
+		this.dataDeInsercao = dataDeInsercao;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [CategoriaId=" + CategoriaId + ", descricao=" + descricao + ", dataDeInsercao="
+				+ dataDeInsercao + "]";
+	}
+	
+	
+	
+	
+}
